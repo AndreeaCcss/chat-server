c83f2e915bebebb4c254c69c77a49badf86a391c
andreeaCcss <andreeaccss@gmail.com>
2019-09-13 11:56:36 +0200

    add cors

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const express = require("express");
 // then you can send a message to the stream and the message will be sent to all the clients
 const Sse = require("json-sse");
 const factory = require("./router");
-
+const cors = require("cors");
 const bodyParser = require("body-parser");
-const JSONparser = bodyParser.json();
 
+const JSONparser = bodyParser.json();
 // quality by value - it makes a new copy when you would export it
 // it would be a different stream if you export it
 // we need to use the same stream - the factory allows that
 const stream = new Sse();
 const app = express();
+const middleware = cors();
+app.use(middleware);
+
 const port = process.env.PORT || 4000;
 
 app.use(JSONparser);
