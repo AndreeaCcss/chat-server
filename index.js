c60f786b085fa347f1df45dfb679ee50152d54ce
andreeaCcss <andreeaccss@gmail.com>
2019-09-13 10:21:57 +0200

    add listening notification

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 4000;
-app.listen(port);
+
+function onListen() {
+  console.log(`Listening on ${port}`);
+}
+
+app.listen(port, onListen);
